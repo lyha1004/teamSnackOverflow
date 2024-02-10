1ab6290f3edf622707f14c720fa457ba77f8264e
kngo44 <131712579+kngo44@users.noreply.github.com>
2024-02-10 17:32:43 -0600

    PD

--- datasets/parse_data.py
+++ datasets/parse_data.py
@@ -3,17 +3,21 @@ import pandas as pd
 excel_file_path = 'Conoco_Phillips.xlsx'
 
 excel_data = pd.read_excel(excel_file_path, engine='openpyxl')  
+df = pd.DataFrame(excel_data)
 
 #json_str = excel_data.to_json()
 
 #print('Excel Data:\n', json_str)
 
-output_path = 'conoco_output.json'
+# output_path = 'conoco_output.json'
 
-for column_name in excel_data.columns:
-    column_data = excel_data[column_name]
+# for column_name in excel_data.columns:
+#     column_data = excel_data[column_name]
 
-    json_str = column_data.to_json()
+#     json_str = column_data.to_json()
 
-    with open(output_path, 'w') as json_file:
-        json_file.write(json_str)
+#     with open(output_path, 'w') as json_file:
+#         json_file.write(json_str)
+
+print(df)
+df.to_json('output.json', orient='columns')
