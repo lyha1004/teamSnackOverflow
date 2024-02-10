f9927594fac0483a86e8a5dd83f744e475e117ce
Ly Ha <lythituyetha@gmail.com>
2024-02-10 17:05:55 -0600

    parsing data by column

--- datasets/parse_data.py
+++ datasets/parse_data.py
@@ -4,6 +4,16 @@ excel_file_path = 'Conoco_Phillips.xlsx'
 
 excel_data = pd.read_excel(excel_file_path, engine='openpyxl')  
 
-json_str = excel_data.to_json()
+#json_str = excel_data.to_json()
 
-print('Excel Data:\n', json_str)
+#print('Excel Data:\n', json_str)
+
+output_path = 'conoco_output.json'
+
+for column_name in excel_data.columns:
+    column_data = excel_data[column_name]
+
+    json_str = column_data.to_json()
+
+    with open(output_path, 'w') as json_file:
+        json_file.write(json_str)
