8f748694861496c6bb70243f615c7a227d07c71a
rphong <richard.phong424@gmail.com>
2024-02-10 20:05:51 -0600

    feat: add globe polygons

--- globe-visualization/src/pages/home.js
+++ globe-visualization/src/pages/home.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { useRef } from 'react'; 
-import blueMarble from '../resources/globeImg.jpg'
+import React, { useEffect, useRef, useState } from 'react';
+import blueMarble from '../resources/globeImg.jpg';
 import Globe from 'react-globe.gl';
+import countriesMap from '../data/worldmap.geojson';
 
 export default function Home() {
-  const globeEl = useRef();
+  const [countries, setCountries] = useState();
 
-  return (
+  useEffect(() => {
+    fetch(countriesMap)
+      .then((res) => res.json())
+      .then((countries) => {
+        setCountries(countries);
+        console.log('countries loaded');
+      });
+  }, []);
+
+  return countries ? (
     <Globe
-      ref={globeEl}
       globeImageUrl={blueMarble}
+      backgroundImageUrl='//unpkg.com/three-globe/example/img/night-sky.png'
+      lineHoverPrecision={0}
+      polygonsData={countries.features.filter(
+        (d) => d.properties.ISO_A2 !== 'AQ'
+      )}
+      polygonAltitude={0.06}
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+      polygonStrokeColor={() => '#111'}
+      polygonLabel='testing'
+      onPolygonHover={(polygon) => console.log('polygon', polygon)}
+      polygonsTransitionDuration={10}
     />
+  ) : (
+    <div>Loading data...</div>
   );
 }
