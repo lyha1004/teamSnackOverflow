90e030575f78b769042162efac86a1480aa87a5e
Ly Ha <lythituyetha@gmail.com>
2024-02-10 20:08:47 -0600

    gui!

---
+++ globe-visualization/src/pages/GUI.js
@@ -0,0 +1,11 @@
+import { useControls } from 'leva'
+
+export default function MyComponent() {
+    const {name, aNumber } = useControls({name : 'World', aNumber: 0})
+
+    return (
+        <div>
+            Hey {name}, hello! {aNumber}
+        </div>
+    )
+}
