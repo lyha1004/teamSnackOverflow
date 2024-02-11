b4a04e11d9cfb008c481e7a6c39f838bb1ec3928
rphong <richard.phong424@gmail.com>
2024-02-11 12:59:24 -0600

    fix: add year to description label

--- globe-visualization/src/component/countryInfo.js
+++ globe-visualization/src/component/countryInfo.js
@@ -3,7 +3,7 @@ import '../style/countryInfo.css';
 export default function CountryInfo({ CountryName, countryDescription }) {
   return CountryName ? (
     <div className='infoPanel'>
-      <h1 className='infoTitle'>{CountryName}</h1>
+      <h1 className='infoTitle'>{CountryName} (2022)</h1>
       <div className='descriptionPannel'>
         <h2 className='infoDescription'>{countryDescription}</h2>
       </div>
